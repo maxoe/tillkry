111963cccba1151108bbe173c3d959c0f93a14b2
Max Oesterle <max_oesterle@web.de>
2020-03-12 16:09:48 +0100

    Speedup to 0.82 by ommitting matrix accesses and allocation

--- aes.c
+++ aes.c
@@ -118,61 +118,43 @@ void mixColumns(unsigned char *state) {
 	
 	memcpy(oldState, state, blockSize);
 
-	size_t mat[4][4] = {{2,3,1,1},{1,2,3,1},{1,1,2,3},{3,1,1,2}};
+	//size_t mat[4][4] = {{2,3,1,1},{1,2,3,1},{1,1,2,3},{3,1,1,2}};
 
 	for (size_t i = 0; i < 4; ++i) {
 		unsigned char a = oldState[0 + i * 4];
 		unsigned char b = oldState[1 + i * 4];
-		// unsigned char c = oldState[2 + i * 4];
-		// unsigned char d = oldState[3 + i * 4];
 
-		galMul(&a, mat[0][0]);
-		galMul(&b, mat[0][1]);
-		// galMul(&c, mat[0][2]);
-		// galMul(&d, mat[0][3]);
+		galMul(&a, 2);
+		galMul(&b, 3);
 
 		state[0 + i * 4] = a ^ b ^ oldState[2 + i * 4] ^ oldState[3 + i * 4];
 
 
-		// a = oldState[0 + i * 4];
 		b = oldState[1 + i * 4];
 		unsigned char c = oldState[2 + i * 4];
-		// d = oldState[3 + i * 4];
 
-		// galMul(&a, mat[1][0]);
-		galMul(&b, mat[1][1]);
-		galMul(&c, mat[1][2]);
-		// galMul(&d, mat[1][3]);
+		galMul(&b, 2);
+		galMul(&c, 3);
 
 		state[1 + i * 4] = oldState[0 + i * 4] ^ b ^ c ^ oldState[3 + i * 4];
 
 
-		// a = oldState[0 + i * 4];
-		// b = oldState[1 + i * 4];
 		c = oldState[2 + i * 4];
 		unsigned char d = oldState[3 + i * 4];
 
-		// galMul(&a, mat[2][0]);
-		// galMul(&b, mat[2][1]);
-		galMul(&c, mat[2][2]);
-		galMul(&d, mat[2][3]);
+		galMul(&c, 2);
+		galMul(&d, 3);
 
 		state[2 + i * 4] = oldState[0 + i * 4] ^ oldState[1 + i * 4] ^ c ^ d;
 
 
 		a = oldState[0 + i * 4];
-		// b = oldState[1 + i * 4];
-		// c = oldState[2 + i * 4];
 		d = oldState[3 + i * 4];
 
-		galMul(&a, mat[3][0]);
-		// galMul(&b, mat[3][1]);
-		// galMul(&c, mat[3][2]);
-		galMul(&d, mat[3][3]);
+		galMul(&a, 3);
+		galMul(&d, 2);
 
-		state[3 + i * 4] = a ^ oldState[1 + i * 4] ^ oldState[2 + i * 4] ^ d;
-		
-		
+		state[3 + i * 4] = a ^ oldState[1 + i * 4] ^ oldState[2 + i * 4] ^ d;		
 	}
 
 	free(oldState);
